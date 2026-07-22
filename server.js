2c4452da4531f9bf1bf6f128aa46c0ee93214244
tinchobecchio <tincho.becchio@gmail.com>
2026-07-22 04:26:11 -0300

    headers y ruta get

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import crypto from 'node:crypto'
 
 dotenv.config()
 const app = express()
@@ -17,6 +18,46 @@ const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Shared authentication function
+function generateAuthHeaders() {
+    const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
+    const hash = crypto.createHash('sha1').update(authKey + secretKey + apiHeaderTime).digest('hex');
+    return {
+        'User-Agent': userAgent,
+        'X-Auth-Key': authKey,
+        'X-Auth-Date': apiHeaderTime.toString(),
+        'Authorization': hash
+    }
+}
+
+// Search for podcasts
+app.get('/api/search', async(req,res) => {
+    const query = req.query.q
+    if (!query) {
+        return res.status(400).json({error: 'Query Parameter is required'})
+    }
+
+    const headers = generateAuthHeaders()
+    try {
+        const response = await fetch(`${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`, {
+            method: 'GET',
+            headers: headers
+        })
+
+        if (response.ok && response.headers.get('content-type').includes('application/json')) {
+            const data = await response.json()
+            res.json(data)
+        } else {
+            const rawText = await response.text()
+            console.log('Raw Response:', rawText);
+            res.status(500).json({error: 'Invalid response from API', rawText})
+        }
+        
+    } catch (error) {
+        console.error('Error fetching API: ', error)
+        res.status(500).json({error: error.message})
+    }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
