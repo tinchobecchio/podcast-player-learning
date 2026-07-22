ff3d0b18aed7938e92bb1f2a414e298cb199bed0
tinchobecchio <tincho.becchio@gmail.com>
2026-07-22 03:39:52 -0300

    configuraciones iniciales del servidor

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello martin')
-})
+const authKey = process.env.AUTH_KEY
+const secretKey = process.env.SECRET_KEY
+const userAgent = process.env.USER_AGENT
+const apiEndpoint = process.env.API_ENDPOINT
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
